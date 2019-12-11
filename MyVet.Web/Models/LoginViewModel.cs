497a6f36b977fe4b928462821fc49dd7ff8d7c93
knenita <diana.russiposada@gmail.com>
2019-12-11 02:05:57 -0500

    LOGIN AND LOGOUT OPTIONS

--- MyVet.Web/Models/LoginViewModel.cs
+++ MyVet.Web/Models/LoginViewModel.cs
@@ -1,11 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace MyVet.Web.Models
 {
     public class LoginViewModel
     {
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        [EmailAddress(ErrorMessage ="Ingrese un correo valido en el campo{0}")]
+        [Display(Name ="Usuario")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        [MinLength(6, ErrorMessage = "El campo {0} debe tener minimo {1} caracteres")]
+        [Display(Name = "Contraseña")]
+        public string Password { get; set; }
+        public bool RememberMe { get; set; }
     }
 }
