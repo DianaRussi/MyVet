ca7f083e999b859aca2f293d2aed5725f7b155de
Diana Russi <diana.russiposada@hotmail.com>
2019-12-21 14:16:18 -0500

    user methods of api

---
+++ MyVet.Web/Controllers/API/AccountController.cs
@@ -0,0 +1,202 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MyVet.Common.Models;
+using MyVet.Web.Data;
+using MyVet.Web.Data.Entities;
+using MyVet.Web.Helpers;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Controllers.API
+{
+    [Route("api/[Controller]")]
+    public class AccountController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+        private readonly IMailHelper _mailHelper;
+
+        public AccountController(
+            DataContext dataContext,
+            IUserHelper userHelper,
+            IMailHelper mailHelper)
+        {
+            _context = dataContext;
+            _userHelper = userHelper;
+            _mailHelper = mailHelper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostUser([FromBody] UserRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response<object>
+                {
+                    IsSuccess = false,
+                    Message = "Bad request"
+                });
+            }
+
+            var user = await _userHelper.GetUserByEmailAsync(request.Email);
+            if (user != null)
+            {
+                return BadRequest(new Response<object>
+                {
+                    IsSuccess = false,
+                    Message = "Este correo ya existe."
+                });
+            }
+
+            user = new User
+            {
+                Address = request.Address,
+                Document = request.Document,
+                Email = request.Email,
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                PhoneNumber = request.Phone,
+                UserName = request.Email
+            };
+
+            var result = await _userHelper.AddUserAsync(user, request.Password);
+            if (result != IdentityResult.Success)
+            {
+                return BadRequest(result.Errors.FirstOrDefault().Description);
+            }
+
+            var userNew = await _userHelper.GetUserByEmailAsync(request.Email);
+            await _userHelper.AddUserToRoleAsync(userNew, "Customer");
+            _context.Owners.Add(new Owner { User = userNew });
+            await _context.SaveChangesAsync();
+
+            var myToken = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+            var tokenLink = Url.Action("ConfirmEmail", "Account", new
+            {
+                userid = user.Id,
+                token = myToken
+            }, protocol: HttpContext.Request.Scheme);
+
+            _mailHelper.SendMail(request.Email, "Email de confirmación", $"<h1>Email de confirmación</h1>" +
+                $"Para activar su usuario, " +
+                $"por favor clic en este link:</br></br><a href = \"{tokenLink}\">Email de confirmación</a>");
+
+            return Ok(new Response<object>
+            {
+                IsSuccess = true,
+                Message = "Se le ha enviado un email a su cuenta de correo. Porfavor confirme para ingresar a la aplicaciòn."
+            });
+        }
+
+        [HttpPost]
+        [Route("RecoverPassword")]
+        public async Task<IActionResult> RecoverPassword([FromBody] EmailRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response<object>
+                {
+                    IsSuccess = false,
+                    Message = "Bad request"
+                });
+            }
+
+            var user = await _userHelper.GetUserByEmailAsync(request.Email);
+            if (user == null)
+            {
+                return BadRequest(new Response<object>
+                {
+                    IsSuccess = false,
+                    Message = "Este correo no existe en el sistema."
+                });
+            }
+
+            var myToken = await _userHelper.GeneratePasswordResetTokenAsync(user);
+            var link = Url.Action("ResetPassword", "Account", new { token = myToken }, protocol: HttpContext.Request.Scheme);
+            _mailHelper.SendMail(request.Email, "Cambio de contraseña", $"<h1>Recuperación de contraseña</h1>" +
+                $"Para cambiar tu contraseña haz clic en el siguiente link:</br></br>" +
+                $"<a href = \"{link}\">RCambiar contraseña</a>");
+
+            return Ok(new Response<object>
+            {
+                IsSuccess = true,
+                Message = "Se ha enviado a tu correo instrucciones para cambiar tu contraseña."
+            });
+        }
+
+        [HttpPut]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> PutUser([FromBody] UserRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userEntity = await _userHelper.GetUserByEmailAsync(request.Email);
+            if (userEntity == null)
+            {
+                return BadRequest("Usuario no encontrado.");
+            }
+
+            userEntity.FirstName = request.FirstName;
+            userEntity.LastName = request.LastName;
+            userEntity.Address = request.Address;
+            userEntity.PhoneNumber = request.Phone;
+            userEntity.Document = request.Phone;
+
+            var respose = await _userHelper.UpdateUserAsync(userEntity);
+            if (!respose.Succeeded)
+            {
+                return BadRequest(respose.Errors.FirstOrDefault().Description);
+            }
+
+            var updatedUser = await _userHelper.GetUserByEmailAsync(request.Email);
+            return Ok(updatedUser);
+        }
+
+        [HttpPost]
+        [Route("ChangePassword")]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordRequest request)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(new Response<object>
+                {
+                    IsSuccess = false,
+                    Message = "Bad request"
+                });
+            }
+
+            var user = await _userHelper.GetUserByEmailAsync(request.Email);
+            if (user == null)
+            {
+                return BadRequest(new Response<object>
+                {
+                    IsSuccess = false,
+                    Message = "El correo ingresado no existe."
+                });
+            }
+
+            var result = await _userHelper.ChangePasswordAsync(user, request.OldPassword, request.NewPassword);
+            if (!result.Succeeded)
+            {
+                return BadRequest(new Response<object>
+                {
+                    IsSuccess = false,
+                    Message = result.Errors.FirstOrDefault().Description
+                });
+            }
+
+            return Ok(new Response<object>
+            {
+                IsSuccess = true,
+                Message = "La contraseña fue cambiada exitosamente!"
+            });
+        }
+    }
+}
