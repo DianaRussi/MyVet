dcd77afe2a5b13daec8c3b533b978f83d5866f58
Diana Russi <diana.russiposada@hotmail.com>
2019-12-24 02:42:45 -0500

    current ubication on map

---
+++ MyVet.Common/Models/Services/IGeolocatorService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace MyVet.Common.Models.Services
+{
+    public interface IGeolocatorService
+    {
+        double Latitude { get; set; } // lo que se aleja o acerca linea del ecuador
+        double Longitude { get; set; }// lo que nos movemos al oriente u occidente del meridiano de grinwich
+        Task GetLocationAsync();
+    }
+}
